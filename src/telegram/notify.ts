779abe44c6859fd5cf8c2bb867835412748b6c70
vlad <0956458@hr.nl>
2022-04-23 14:30:27 +0200

    Reformatted with prettier

--- src/telegram/notify.ts
+++ src/telegram/notify.ts
@@ -1,78 +1,77 @@
 import { cinemas, PatheCinema } from "../cinemas"
 import { bot } from "./bot"
 
-
 type LocationNames = Omit<PatheCinema, "id">
 const notifyBot = () => {
-    const cinemasHash = new Map<number, LocationNames>()
-    bot.on('callback_query', async (callback) => {
+  const cinemasHash = new Map<number, LocationNames>()
+  bot.on("callback_query", async (callback) => {
+    //Cant get the id
+    const movieTitle = callback.message.caption
 
-        //Cant get the id
-        const movieTitle = callback.message.caption
+    const action = callback.data
+    const chatId = callback.message.chat.id
+    const messageId = callback.message.message_id
+    const cinemaIds = cinemas.map((e) => `select-${e.id}`)
+    const allowedActions: string[] = ["notify", "confirm", ...cinemaIds]
 
-        const action = callback.data
-        const chatId = callback.message.chat.id
-        const messageId = callback.message.message_id
-        const cinemaIds = cinemas.map(e => `select-${e.id}`)
-        const allowedActions: string[] = ['notify', 'confirm', ...cinemaIds]
+    //TODO: parse id. Can this be done better
+    const lastSelectedId = parseInt(action.split("-").reverse()[0])
+    if (!allowedActions.includes(action)) return
 
-        //TODO: parse id. Can this be done better
-        const lastSelectedId = parseInt(action.split('-').reverse()[0])
-        if (!allowedActions.includes(action)) return
-
-        //TODO: Can you do this with string literals?
-        cinemaIds.map(elem => {
-            switch (action) {
-                case elem:
-                    console.log(cinemaIds)
-                    bot.editMessageReplyMarkup({ inline_keyboard: [...getKeyboard(lastSelectedId, cinemasHash), [{ text: '✅', callback_data: 'action, 39, 28' }]] }, { chat_id: chatId, message_id: messageId })
-                    break;
-            }
-        }
-        )
-        switch (action) {
-            case 'notify':
-                bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id: chatId, message_id: messageId })
-
-                bot.sendMessage(chatId, 'Please select one or more cinemas', {
-                    reply_markup: {
-                        // TODO: Pas callback_data van confirm aan
-                        inline_keyboard:
-                            [...getKeyboard(-1, cinemasHash), [{ text: '✅', callback_data: 'action, 39, 28' }]],
-                    }
-                })
-                break;
-            case 'confirm':
-                break;
-            default:
-                break;
-        }
+    //TODO: Can you do this with string literals?
+    cinemaIds.map((elem) => {
+      switch (action) {
+        case elem:
+          bot.editMessageReplyMarkup(
+            {
+              inline_keyboard: [
+                ...getKeyboard(lastSelectedId, cinemasHash),
+                [{ text: "✅", callback_data: "action, 39, 28" }],
+              ],
+            },
+            { chat_id: chatId, message_id: messageId }
+          )
+          break
+      }
     })
+    switch (action) {
+      case "notify":
+        bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id: chatId, message_id: messageId })
+
+        bot.sendMessage(chatId, "Please select one or more cinemas", {
+          reply_markup: {
+            // TODO: Pas callback_data van confirm aan
+            inline_keyboard: [...getKeyboard(-1, cinemasHash), [{ text: "✅", callback_data: "action, 39, 28" }]],
+          },
+        })
+        break
+      case "confirm":
+        break
+      default:
+        break
+    }
+  })
 }
 
 // todo: refactor to Option<string>
 const getKeyboard = (selectedId: number, cinemasmap: Map<number, LocationNames>) => {
-    console.log(cinemasmap)
-    console.log(selectedId)
-    cinemasmap.has(selectedId) ? cinemasmap.delete(selectedId) : cinemasmap.set(selectedId, cinemas.at(selectedId))
-    const keyboard = cinemas.reduce((arr, curr, ind) => {
-        const res = {
-            text: `${cinemasmap.has(curr.id) ? '✅ ' : ''}${curr.name}`,
-            callback_data: `select-${curr.id}`
-        }
-        if (ind % 2 == 0) {
-            arr.push([res])
-            return arr
-        }
+  cinemasmap.has(selectedId) ? cinemasmap.delete(selectedId) : cinemasmap.set(selectedId, cinemas.at(selectedId))
+  const keyboard = cinemas.reduce((arr, curr, ind) => {
+    const res = {
+      text: `${cinemasmap.has(curr.id) ? "✅ " : ""}${curr.name}`,
+      callback_data: `select-${curr.id}`,
+    }
+    if (ind % 2 == 0) {
+      arr.push([res])
+      return arr
+    }
 
-        //TODO: improve this.
-        arr[arr.length - 1] = [{ ...arr[arr.length - 1][0] }, { ...res }]
+    //TODO: improve this.
+    arr[arr.length - 1] = [{ ...arr[arr.length - 1][0] }, { ...res }]
 
-        return arr
-    }, [])
+    return arr
+  }, [])
 
-    console.log(keyboard[0])
-
-    return keyboard
+  return keyboard
 }
 export default notifyBot
--- src/telegram/search.ts
+++ src/telegram/search.ts
@@ -2,72 +2,73 @@ import { InlineKeyboardButton } from "node-telegram-bot-api"
 import { Movie, searchFilms } from "../scraper/search"
 import { bot } from "./bot"
 
-
-const backButton: InlineKeyboardButton = { text: 'Back', callback_data: 'prev' }
-const nextButton: InlineKeyboardButton = { text: 'Next', callback_data: 'next' }
-const notifyButton: InlineKeyboardButton = { text: 'Notify when available', callback_data: 'notify' }
-const agendaButton: InlineKeyboardButton = { text: 'Agenda', callback_data: 'agenda' }
+const backButton: InlineKeyboardButton = { text: "Back", callback_data: "prev" }
+const nextButton: InlineKeyboardButton = { text: "Next", callback_data: "next" }
+const notifyButton: InlineKeyboardButton = { text: "Notify when available", callback_data: "notify" }
+const agendaButton: InlineKeyboardButton = { text: "Agenda", callback_data: "agenda" }
 
 //TODO: improve
 const getState = (index: number, array_size: number): InlineKeyboardButton[][] => {
-    const current_state = []
-    if (index > 0) {
-        current_state.push(backButton)
-    }
-    if (index < array_size) {
-        current_state.push(nextButton)
-    }
-    return [current_state, [notifyButton, agendaButton]]
+  const current_state = []
+  if (index > 0) {
+    current_state.push(backButton)
+  }
+  if (index < array_size) {
+    current_state.push(nextButton)
+  }
+  return [current_state, [notifyButton, agendaButton]]
 }
 
 const searchBot = () => {
-    let results: Movie[] = []
-    let index = 0
-    bot.onText(/\/search (.+)/, async (msg, match) => {
-        const chatId = msg.chat.id
-        const query = match[1]
-        results = await searchFilms(query)
-        bot.sendPhoto(chatId, results[index].posterUrl, {
-            caption: results[index].name,
+  let results: Movie[] = []
+  let index = 0
+  bot.onText(/\/search (.+)/, async (msg, match) => {
+    const chatId = msg.chat.id
+    const query = match[1]
+    results = await searchFilms(query)
+    bot.sendPhoto(chatId, results[index].posterUrl, {
+      caption: results[index].name,
+      reply_markup: {
+        inline_keyboard: getState(index, results.length - 1),
+      },
+    })
+  })
+
+  bot.on("callback_query", async (callback) => {
+    const chatId = callback.message.chat.id
+    const messageId = callback.message.message_id
+    //TODO: improve index increment and decrements
+    switch (callback.data) {
+      case "prev":
+        index--
+        bot.editMessageMedia(
+          { media: results[index].posterUrl, type: "photo", caption: results[index].name },
+          {
+            chat_id: chatId,
+            message_id: messageId,
             reply_markup: {
-                inline_keyboard: getState(index, results.length - 1)
-            }
-        })
-
-    })
-
-    bot.on("callback_query", async (callback) => {
-
-        const chatId = callback.message.chat.id
-        const messageId = callback.message.message_id
-        //TODO: improve index increment and decrements
-        switch (callback.data) {
-            case 'prev':
-                index--;
-                bot.editMessageMedia({ media: results[index].posterUrl, type: 'photo', caption: results[index].name }, {
-                    chat_id: chatId,
-                    message_id: messageId,
-                    reply_markup: {
-                        inline_keyboard: getState(index, results.length - 1)
-                    }
-                })
-                break;
-            case 'next':
-                index++;
-                bot.editMessageMedia({ media: results[index].posterUrl, type: 'photo', caption: results[index].name }, {
-                    chat_id: chatId,
-                    message_id: messageId,
-                    reply_markup: {
-                        inline_keyboard: getState(index, results.length - 1)
-                    }
-                })
-                break;
-            default:
-                break;
-        }
-
-    })
+              inline_keyboard: getState(index, results.length - 1),
+            },
+          }
+        )
+        break
+      case "next":
+        index++
+        bot.editMessageMedia(
+          { media: results[index].posterUrl, type: "photo", caption: results[index].name },
+          {
+            chat_id: chatId,
+            message_id: messageId,
+            reply_markup: {
+              inline_keyboard: getState(index, results.length - 1),
+            },
+          }
+        )
+        break
+      default:
+        break
+    }
+  })
 }
 
 export default searchBot
-
