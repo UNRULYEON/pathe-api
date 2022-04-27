755e99d4ad4bdd1c6d5747960daa73ee5d854faa
vlad <0956458@hr.nl>
2022-04-27 19:58:28 +0200

    Take the wheel

--- src/cronjob/notifier.ts
+++ src/cronjob/notifier.ts
@@ -13,14 +13,22 @@ const notifier = async () => {
       orderBy: { _count: { movieId: "desc" } },
     })
     .then((ids) => ids.map((id) => id.movieId))
+
   const availableLocations = await Promise.all(
     movieIds.map(async (id) => ({ movieId: id, locations: await getAvailableLocationsById(id) }))
   ).then((allAvailableLocation) => allAvailableLocation.filter((movie) => movie.locations.length > 0))
-  const notificationsByLocation = availableLocations.reduce((acc, movie) => {
-    const locationId = movie.locations.flatMap((e) => cinemasWithFullNames.filter((k) => k.name === e))[0].id
+
+  const moviesWithLocations: { movieId: number; location_id: number }[] = availableLocations.reduce((acc, movie) => {
+    const locationId = movie.locations.flatMap((location) =>
+      cinemasWithFullNames.filter((cinema) => cinema.name === location)
+    )[0].id
     return [...acc, { movieId: movie.movieId, location_id: locationId }]
   }, [])
-  console.log(notificationsByLocation)
+  await Promise.all(
+    moviesWithLocations.map(async (ml) => {
+      const result = await prisma.telegramNotification.findMany({ where: { movieId: ml.movieId } })
+    })
+  )
 }
 
 export default notifier
