3e88f694bbe3effa28e134b8ae7fafe57e56974e
vlad <0956458@hr.nl>
2022-04-27 17:22:59 +0200

    Added endpoint to see available locations

---
+++ src/scraper/movie.ts
@@ -0,0 +1,40 @@
+import * as cheerio from "cheerio"
+
+import axios from "axios"
+import prisma from "../prisma/context"
+
+const BASE_URL = "https://www.pathe.nl"
+
+const getQueryString = (movieId: string, cinemaIds: number[]): string =>
+  `film/${movieId}/agenda?${cinemaIds.reduce((acc, id) => acc + `cinemas=${id}&`, "")}`
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+})
+
+const getAvailableLocationsById = async (movieId: string): Promise<any> => {
+  const cinemaIds = await prisma.cinema
+    .findMany({ select: { patheId: true } })
+    .then((cinemas) => cinemas.map((cinema) => cinema.patheId))
+
+  const result = await instance.get(getQueryString(movieId, cinemaIds)).then(({ data }) => {
+    const $ = cheerio.load(data)
+
+    if ($("body > div").length === 0) return []
+
+    const availableLocation: string[] = []
+    const locationNodes = $("body > div > div > div > div > p")
+
+    locationNodes.each((_, locationElement) => {
+      const location = $(locationElement).text()
+
+      if (!availableLocation.includes(location)) availableLocation.push(location)
+    })
+
+    return availableLocation
+  })
+
+  return result
+}
+
+export default getAvailableLocationsById
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from "express"
 import { getUpcomingFilms } from "./scraper/upcoming"
 import { searchFilms } from "./scraper/search"
 import initBot from "./telegram/bot"
+import getAvailableLocationsById from "./scraper/movie"
 
 const app = express()
 
@@ -16,10 +17,16 @@ app.get("/search", async (req, res) => {
 })
 
 app.get("/upcoming", async (req, res) => {
-  let films = await getUpcomingFilms()
+  const films = await getUpcomingFilms()
   res.send(films)
 })
 
+app.get("/locations_by_movie_id", async (req, res) => {
+  if (!req.query.id) return res.sendStatus(400)
+
+  const locations = await getAvailableLocationsById(req.query.id as string)
+  res.send(locations)
+})
 initBot()
 
 app.listen(4000, () => console.log("Listening on port 4000"))
